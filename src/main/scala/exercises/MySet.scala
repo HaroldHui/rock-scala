658e4d9998e2d9a7f42759e92b9410f88dbf90d5
Shu Hui <dreamlock0630@gmail.com>
2019-05-16 00:47:15 +1000

    finsh set

--- src/main/scala/exercises/MySet.scala
+++ src/main/scala/exercises/MySet.scala
@@ -1,5 +1,7 @@
 package exercises
 
+import scala.annotation.tailrec
+
 trait MySet[A] extends (A => Boolean) {
 
   def apply(elem: A): Boolean =
@@ -42,9 +44,40 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
   def ++(anotherSet: MySet[A]): MySet[A] =
     tail ++ anotherSet + head
 
-
   def map[B](f: A => B): MySet[B] = (tail map f) + f(head)
-  def flatMap[B](f: A => MySet[B]): MySet[B]
-  def filter(predicate: A => Boolean): MySet[A]
-  def foreach(f: A => Unit): Unit
+  def flatMap[B](f: A => MySet[B]): MySet[B] = (tail flatMap f) ++ f(head)
+  def filter(predicate: A => Boolean): MySet[A] = {
+    val filteredTail = tail filter predicate
+    if (predicate(head)) filteredTail + head
+    else filteredTail
+  }
+  def foreach(f: A => Unit): Unit = {
+    f(head)
+    tail foreach f
+  }
+}
+
+object MySet{
+  /*
+    val s = MySet(1,2,3) = buildSet(Seq(1,2,3), [])
+     = buildSet(seq(2,3), [] + 1)
+     = buildSet(seq(3). [1] + 2)
+     = buildSet(seq(), [1,2] + 3)
+     = [1,2,3]
+  */
+  def apply[A](values: A*): MySet[A] = {
+    @tailrec
+    def buildSet(valSeq: Seq[A], acc: MySet[A]): MySet[A] =
+      if (valSeq.isEmpty) acc
+      else buildSet(valSeq.tail, acc + valSeq.head)
+
+    buildSet(values.toSeq, new EmptySet[A])
+  }
+}
+
+object MySetPlayground extends App {
+  val s = MySet(1,2,3,4)
+//  s + 5 ++ MySet(-1,-2) + 3 map (x => x * 10) foreach println
+  println("")
+  s + 5 ++ MySet(-1,-2) + 3 flatMap (x => MySet(x * 10)) filter (_ % 2 == 0) foreach println
 }
